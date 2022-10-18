a1c59afd3e7c5f20125d8c7640fc45c1cf98fe4c
Brandon Chan <bchan26@bu.edu>
2022-10-18 13:53:48 -0400

    Successfully created data output with either metric or imperial units via 2 separate start buttons and 2 separate fetch async functions AKA applying the math conversions before the new table is created, instead of after the fact. This avoids the necessity of changing or adding classes to a local element within the async function. Because of this, I needed to remove the enter button event click listeners.

--- script.js
+++ script.js
@@ -1,34 +1,32 @@
-//Global Variable for API Call
-//created variables, connecting HTML's ID to Javascript
-let searchButton = document.querySelector("#searchButton")
+/////////Globally created variables, connecting HTML's ID to Javascript/////////
+let usaButton = document.querySelector("#usaButton")
 let textLatitude = document.querySelector("#latitude")
 let textLongitude = document.querySelector("#longitude")
+let metricButton = document.querySelector("#metricButton")
 
 
 
-///////////////////////Geolocation//////////////////////////
-
+///////////////////////////////Geolocation//////////////////////////////////
 //https://developer.mozilla.org/en-US/docs/Web/API/GeolocationCoordinates/longitude
-//https://developer.mozilla.org/en-US/docs/Web/API/Navigator/geolocation
-//function uses navigator.geolocation to access location data, with the method "getCurrentPosition"
+//Uses navigator.geolocation to get location, with method "getCurrentPosition"
 const locationFinder = () => {
     navigator.geolocation.getCurrentPosition((position) => {
         let calculatedLatAndLong = document.querySelector("#calculatedLatAndLong")
         calculatedLatAndLong.innerText = `Your latitude is ${position.coords.latitude} & longitude is ${position.coords.longitude}.`
-        //The following 2 lines change the value inside of the lat/long input boxes
+        //These 2 lines change the value inside of the lat/long input boxes
         textLatitude.value = position.coords.latitude
         textLongitude.value = position.coords.longitude
     })
 }
-//variable to connect button to Javascript code, with a click event listener to execute the function abov
+//variable to connect button to Javascript, with event listener to execute the function
 let myLocation = document.querySelector("#location")
 myLocation.addEventListener("click", locationFinder)
 
 
 
-////////////////////////Toggling Map//////////////////////////
-//Function to show latitude/longitude map
+/////////////////////////////Toggling Map//////////////////////////////////////
 //https://www.w3schools.com/jsref/prop_style_display.asp
+//Function to show latitude/longitude map
 const showMap = () => {
     document.querySelector("#map").style.display = `flex`
 }
@@ -48,52 +46,54 @@ toggleMapSwitch.addEventListener("click", hideMap)
 
 
 
-///////////////////Toggling Hourly Table////////////////////////
+/////////////////////////////Toggling Hourly Table/////////////////////////////
+//Function to show hourly table
 const showHourly = () => {
     document.querySelector("#hourly").style.display = `block`
 }
 
-//Show map button (which has to come after function)
+//Show hourly button (which has to come after function)
 let hourlyToggleShow = document.querySelector("#hourlyShow")
 hourlyToggleShow.addEventListener("click", showHourly)
 
-//Function to hide latitude/longitude map
+//Function to hide hourly table
 const hideHourly = () => {
     document.querySelector("#hourly").style.display = `none`
 }
 
-//Hide map button (which has to come after function)
+//Hide hourly button (which has to come after function)
 let hourlyToggleHide = document.querySelector("#hourlyHide")
 hourlyToggleHide.addEventListener("click", hideHourly)
 
 
 
-//////////////////Toggling 7-Day Table/////////////////////////
+///////////////////////////Toggling 7-Day Table//////////////////////////////
+//Function to show 7 day table
 const showSevenDay = () => {
     document.querySelector("#sevenDay").style.display = `block`
 }
 
-//Show map button (which has to come after function)
+//Show 7 day table (which has to come after function)
 let sevenToggleShow = document.querySelector("#sevenShow")
 sevenToggleShow.addEventListener("click", showSevenDay)
 
-//Function to hide latitude/longitude map
+//Function to hide 7 day table
 const hideSevenDay = () => {
     document.querySelector("#sevenDay").style.display = `none`
 }
 
-//Hide map button (which has to come after function)
+//Hide 7 Day Table (which has to come after function)
 let sevenToggleHide = document.querySelector("#sevenHide")
 sevenToggleHide.addEventListener("click", hideSevenDay)
 
 
 
-////////////////////////API Call for Weather Data//////////////////////////
+//////////////////////////////IMPERIAL API CALL////////////////////////////////
 
-//function to connect to API and display relevant data
-async function getData () {
+//function to connect to API and display relevant data for IMPERIAL
+async function getImperialData () {
     
-    //Fetches - Includes temp, sunrise, sunset, rain, snow (in F, Inches, Auto-Time Zone)
+    //Fetches from https://open-meteo.com/en/docs
     fetch(`https://api.open-meteo.com/v1/forecast?latitude=${textLatitude.value}&longitude=${textLongitude.value}&hourly=temperature_2m,relativehumidity_2m,rain,snowfall,cloudcover_low,windspeed_10m&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset,rain_sum,snowfall_sum,windspeed_10m_max,winddirection_10m_dominant&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timezone=auto`)
 
     //needed, as mentioned in the mini-lesson
@@ -101,25 +101,23 @@ async function getData () {
         return res.json()
     })
 
-    // All data API calls & Text Display on HTML
+    // All data API calls & DOM Text Display on HTML
     .then(res => {
 
         //console log to get full API call
         console.log(res)
 
         //Time Zone
-        console.log(`Time Zone: `, res.timezone)
         let timeZone = document.querySelector("#timezone")
         timeZone.innerText = res.timezone
-        
-        //Elevation in meters
-        console.log(`Elevation (m): `, res.elevation)
-        let elevation = document.querySelector("#elevation")
-        elevation.innerText = res.elevation
 
-        //7 day forecast data table, referencing https://www.youtube.com/watch?v=XmdOZ5NSqb8 to create a new row each time and using .innerHTML to recognize row/cells being added, instead of just innertext
-        //https://www.w3schools.com/js/js_string_methods.asp for slice to clean up text for date/sunrise/sunset
-        const addCellValues = () => {
+        //Forecast Data Tables
+        //Referenced https://www.youtube.com/watch?v=XmdOZ5NSqb8
+        //Learned to create a new row each time, using .innerHTML to recognize the row/cells being added (instead of just using innertext)
+        //Referenced https://www.w3schools.com/js/js_string_methods.asp
+        //Learned to slice to clean up text for date/sunrise/sunset
+        //Robert from class also helped me brainstorm to apply math to values as needed
+        const myWeatherImperial = () => {
             
             //Just Today's Forecast
             let justTodayTable = document.querySelector("#todayOnly")
@@ -128,87 +126,181 @@ async function getData () {
             for (let k=0; k<1; k++) {
                 let todaysRow =`<tr>
                                 <td>${res.daily.time[k].slice(5)}</td>
-                                <td>${res.daily.temperature_2m_max[k]}</td>
-                                <td>${res.daily.temperature_2m_min[k]}</td>
+                                <td>${res.daily.temperature_2m_max[k]} (°F)</td>
+                                <td>${res.daily.temperature_2m_min[k]} (°F)</td>
                                 <td>${res.daily.sunrise[k].slice(11)}</td>
                                 <td>${res.daily.sunset[k].slice(11)}</td>
-                                <td>${res.daily.rain_sum[k]}</td>
-                                <td>${res.daily.snowfall_sum[k]}</td>
-                                <td>${res.daily.windspeed_10m_max[k]}</td>
-                                <td>${res.daily.winddirection_10m_dominant[k]}</td>
+                                <td>${res.daily.rain_sum[k]} (in)</td>
+                                <td>${res.daily.snowfall_sum[k]*0.3937} (in)</td>
+                                <td>${res.daily.windspeed_10m_max[k]} (mph)</td>
+                                <td>${res.daily.winddirection_10m_dominant[k]}°</td>
                                 </tr>`
                 justTodayTable.innerHTML += todaysRow
             }
-            console.log(justTodayTable.innerHTML)
 
-
-            //Hourly Forecast Code
+            //Hourly Forecast
             let hourlyTable = document.querySelector("#hourlyAll")
             hourlyTable.innerHTML = ``
 
-            for (let j=0; j < 25; j++) {
+            for (let j=0; j < 24; j++) {
                 let hourlyRows=`<tr>
                                 <td>${res.hourly.time[j].slice(11)}</td>
-                                <td>${res.hourly.temperature_2m[j]}</td>
-                                <td>${res.hourly.cloudcover_low[j]}</td>
-                                <td>${res.hourly.relativehumidity_2m[j]}</td>
-                                <td>${res.hourly.rain[j]}</td>
-                                <td>${res.hourly.snowfall[j]}</td>
-                                <td>${res.hourly.windspeed_10m[j]}</td>
+                                <td>${res.hourly.temperature_2m[j]} (°F)</td>
+                                <td>${res.hourly.cloudcover_low[j]} (%)</td>
+                                <td>${res.hourly.relativehumidity_2m[j]} (%)</td>
+                                <td>${res.hourly.rain[j]} (in)</td>
+                                <td>${res.hourly.snowfall[j]*0.3937} (in)</td>
+                                <td>${res.hourly.windspeed_10m[j]} (mph)</td>
                                 </tr>`
                 hourlyTable.innerHTML += hourlyRows
             }
-            console.log(hourlyTable.innerHTML)
 
-
-
-            //7 Day Forecast Code
+            //7 Day Forecast
             let dataTable = document.querySelector("#cellAll")
             dataTable.innerHTML = ``
 
             for (let i=0; i < res.daily.time.length; i ++) {
                 let rows = `<tr>
                             <td>${res.daily.time[i].slice(5)}</td>
-                            <td>${res.daily.temperature_2m_max[i]}</td>
-                            <td>${res.daily.temperature_2m_min[i]}</td>
+                            <td>${res.daily.temperature_2m_max[i]} (°F)</td>
+                            <td>${res.daily.temperature_2m_min[i]} (°F)</td>
                             <td>${res.daily.sunrise[i].slice(11)}</td>
                             <td>${res.daily.sunset[i].slice(11)}</td>
-                            <td>${res.daily.rain_sum[i]}</td>
-                            <td>${res.daily.snowfall_sum[i]}</td>
-                            <td>${res.daily.windspeed_10m_max[i]}</td>
-                            <td>${res.daily.winddirection_10m_dominant[i]}</td>
+                            <td>${res.daily.rain_sum[i]} (in)</td>
+                            <td>${res.daily.snowfall_sum[i]*0.3937} (in)</td>
+                            <td>${res.daily.windspeed_10m_max[i]} (mph)</td>
+                            <td>${res.daily.winddirection_10m_dominant[i]}°</td>
                             </tr>`
                 dataTable.innerHTML += rows
             }
-            console.log(dataTable.innerHTML)
             }
-        addCellValues()
+        myWeatherImperial()
     })
-
     //needed for errors, as discussed in the mini-lesson
     .catch(err => {
         console.log(`Error!`, err)
     })
 }
 
-//search button, upon click runs the getData function for results, comes after
-searchButton.addEventListener("click", getData)
-
-//////////////Event Listener for Enter Button/////////////
-//https://www.w3schools.com/HOWTO/howto_js_trigger_button_enter.asp
-
-//event listener for hitting the enter button, associated with the longitude box
-textLongitude.addEventListener("keypress", (event) => {
-    if (event.key === "Enter") {
-        event.preventDefault()
-        getData()
-    }
+//search button, upon click runs the getImperialData function for results, comes after
+usaButton.addEventListener("click", () => {
+    getImperialData()
 })
 
-//event listener for hitting the enter button, associated with the latitude box
-textLatitude.addEventListener("keypress", (event) => {
-    if (event.key === "Enter") {
-        event.preventDefault()
-        getData()
-    }
+///////////////////////////////METRIC API CALL/////////////////////////////////////
+//function to connect to API and display relevant data for METRIC
+async function getMetricData () {
+    
+    //Fetches from https://open-meteo.com/en/docs
+    fetch(`https://api.open-meteo.com/v1/forecast?latitude=${textLatitude.value}&longitude=${textLongitude.value}&hourly=temperature_2m,relativehumidity_2m,rain,snowfall,cloudcover_low,windspeed_10m&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset,rain_sum,snowfall_sum,windspeed_10m_max,winddirection_10m_dominant&temperature_unit=celsius&windspeed_unit=kmh&precipitation_unit=mm&timezone=auto`)
+
+    //needed, as mentioned in the mini-lesson
+    .then (res => {
+        return res.json()
+    })
+
+    // All data API calls & DOM Text Display on HTML
+    .then(res => {
+
+        //console log to get full API call
+        console.log(res)
+
+        //Time Zone
+        let timeZone = document.querySelector("#timezone")
+        timeZone.innerText = res.timezone
+
+        //Forecast Data Tables
+        //Referenced https://www.youtube.com/watch?v=XmdOZ5NSqb8
+        //Learned to create a new row each time, using .innerHTML to recognize the row/cells being added (instead of just using innertext)
+        //Referenced https://www.w3schools.com/js/js_string_methods.asp
+        //Learned to slice to clean up text for date/sunrise/sunset
+        //Robert from class also helped me brainstorm to apply math to values as needed
+        const myWeatherImperial = () => {
+            
+            //Just Today's Forecast
+            let justTodayTable = document.querySelector("#todayOnly")
+            justTodayTable.innerHTML = ``
+
+            for (let k=0; k<1; k++) {
+                let todaysRow =`<tr>
+                                <td>${res.daily.time[k].slice(5)}</td>
+                                <td>${res.daily.temperature_2m_max[k]} (°C)</td>
+                                <td>${res.daily.temperature_2m_min[k]} (°C)</td>
+                                <td>${res.daily.sunrise[k].slice(11)}</td>
+                                <td>${res.daily.sunset[k].slice(11)}</td>
+                                <td>${res.daily.rain_sum[k]*0.1} (cm)</td>
+                                <td>${res.daily.snowfall_sum[k]} (cm)</td>
+                                <td>${res.daily.windspeed_10m_max[k]} (km/h)</td>
+                                <td>${res.daily.winddirection_10m_dominant[k]}°</td>
+                                </tr>`
+                justTodayTable.innerHTML += todaysRow
+            }
+
+            //Hourly Forecast
+            let hourlyTable = document.querySelector("#hourlyAll")
+            hourlyTable.innerHTML = ``
+
+            for (let j=0; j < 24; j++) {
+                let hourlyRows=`<tr>
+                                <td>${res.hourly.time[j].slice(11)}</td>
+                                <td>${res.hourly.temperature_2m[j]} (°C)</td>
+                                <td>${res.hourly.cloudcover_low[j]} (%)</td>
+                                <td>${res.hourly.relativehumidity_2m[j]} (%)</td>
+                                <td>${res.hourly.rain[j]*0.1} (cm)</td>
+                                <td>${res.hourly.snowfall[j]} (cm)</td>
+                                <td>${res.hourly.windspeed_10m[j]} (km/h)</td>
+                                </tr>`
+                hourlyTable.innerHTML += hourlyRows
+            }
+
+            //7 Day Forecast
+            let dataTable = document.querySelector("#cellAll")
+            dataTable.innerHTML = ``
+
+            for (let i=0; i < res.daily.time.length; i ++) {
+                let rows = `<tr>
+                            <td>${res.daily.time[i].slice(5)}</td>
+                            <td>${res.daily.temperature_2m_max[i]} (°C)</td>
+                            <td>${res.daily.temperature_2m_min[i]} (°C)</td>
+                            <td>${res.daily.sunrise[i].slice(11)}</td>
+                            <td>${res.daily.sunset[i].slice(11)}</td>
+                            <td>${res.daily.rain_sum[i]*0.1} (cm)</td>
+                            <td>${res.daily.snowfall_sum[i]} (cm)</td>
+                            <td>${res.daily.windspeed_10m_max[i]} (km/h)</td>
+                            <td>${res.daily.winddirection_10m_dominant[i]}°</td>
+                            </tr>`
+                dataTable.innerHTML += rows
+            }
+            }
+        myWeatherImperial()
+    })
+    //needed for errors, as discussed in the mini-lesson
+    .catch(err => {
+        console.log(`Error!`, err)
+    })
+}
+
+//search button, upon click runs the getMetricData function for results, comes after
+metricButton.addEventListener("click", () => {
+    getMetricData()
 })
+
+
+// //////////////Event Listener for Enter Button & Imperial/////////////////////
+// //https://www.w3schools.com/HOWTO/howto_js_trigger_button_enter.asp
+
+// //Event listener for hitting enter button, associated with the longitude box
+// textLongitude.addEventListener("keypress", (event) => {
+//     if (event.key === "Enter") {
+//         event.preventDefault()
+//         getImperialData()
+//     }
+// })
+
+// //Event listener for hitting enter button, associated with the latitude box
+// textLatitude.addEventListener("keypress", (event) => {
+//     if (event.key === "Enter") {
+//         event.preventDefault()
+//         getImperialData()
+//     }
+// })
